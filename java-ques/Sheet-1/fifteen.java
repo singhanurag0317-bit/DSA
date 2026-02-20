3bc50784d43e505a2d441c16819344e9ae30adbf
Anurag Singh <singhanurag0317@gmail.com>
2026-02-20 08:46:30 +0530

    new codes

--- java-ques/Sheet-1/fifteen.java
+++ java-ques/Sheet-1/fifteen.java
@@ -1,12 +1,18 @@
 public class fifteen {
     public static void main(String[] args) {
-        int num = 57944772;
-        int count =0;
-        num = Math.abs(num);
-            while(num>0){
-            count+=1;
-            num=num/10;
+       int num = 456987;
+       num = Math.abs(num);
+       int count=0;
+       if(num==0){
+        count = 1;
+
+       }
+       else{
+        while(num>0){
+            num = num/10;
+            count++;
         }
+       }
         System.out.println(count);
        
     }
--- java-ques/Sheet-1/ten.java
+++ java-ques/Sheet-1/ten.java
@@ -1,1 +1,24 @@
-// fuck hogya
+public class ten{
+    public static void main(String[] args) {
+        int num = 13;
+        boolean isPrime = true;
+
+        if(num<=1){
+            isPrime = false;
+        }
+        else{
+            for(int i =2;i<num;i++){
+                if(num%i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if(isPrime){
+            System.out.println("yes"+num);
+        }
+        else{
+            System.out.println("no"+num);
+        }
+    }
+}
